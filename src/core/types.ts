a21d80abb11119a3783c894d1f5fa214a6963cff
s3xysteak <86149525+s3xysteak@users.noreply.github.com>
2024-07-28 07:08:12 +0800

    docs: add comments.

--- src/core/types.ts
+++ src/core/types.ts
@@ -24,19 +24,43 @@ export interface UseFetch {
 }
 
 export interface UseFetchOptions<R extends ResponseType> extends UseFetchReactiveOptions, Omit<FetchOptions<R>, keyof UseFetchReactiveOptions> {
+  /** If make a request during initialization */
   immediate?: boolean
+
+  /** Same as Vue's `watch`. Refresh when watch source updated */
   watch?: Arrayable<WatchSource> | false
+
+  /** Indicate the interval time for polling in millisecond */
   pollingInterval?: MaybeRefOrGetter<number>
+
+  /** Indicate the debounce delay time in millisecond */
   debounceInterval?: MaybeRefOrGetter<number>
+
+  /** Indicate the throttle wait time in millisecond */
   throttleInterval?: MaybeRefOrGetter<number>
 }
 
 export interface UseFetchReturns<R extends ResponseType, T> {
+  /** The data which `ofetch` returns */
   data: Ref<MappedResponseType<R, T> | null>
+
+  /** A boolean value indicating whether the data is still being fetched */
   pending: Ref<boolean>
+
+  /** A function used to manually trigger the request */
   refresh: () => Promise<void>
+
+  /** A function used to manually trigger the request */
   execute: () => Promise<void>
+
+  /** The error object if the data fetch fails, otherwise `null`. */
   error: Ref<Error | null>
+
+  /**
+   * A string representing the state of the data request.
+   * @example
+   * 'idle', 'pending', 'success', 'error'
+   */
   status: Ref<UseFetchStatus>
 }
 
@@ -67,9 +91,13 @@ export interface UsePaginationOptions<R extends ResponseType> extends UseFetchOp
 }
 
 export interface UsePaginationReturns<R extends ResponseType, T> extends UseFetchReturns<R, T> {
+  /** A number to indicate which page you are on */
   pageCurrent: Ref<number>
+  /** A number to indicate the size of a page */
   pageSize: Ref<number>
 
+  /** Readonly number to indicate how many items of the data */
   total: ComputedRef<number>
+  /** Readonly number to indicate how many pages here */
   pageTotal: ComputedRef<number>
 }
